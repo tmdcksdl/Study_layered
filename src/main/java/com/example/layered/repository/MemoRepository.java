acf56960811bc5eb244e38a19e3fd35d4c25a2f5
tmdcksdl <tmdcksdl99@naver.com>
2024-12-04 11:18:05 +0900

    :sparkles: Fear saveMemo Method

--- src/main/java/com/example/layered/repository/MemoRepository.java
+++ src/main/java/com/example/layered/repository/MemoRepository.java
@@ -1,4 +1,13 @@
 package com.example.layered.repository;
 
+import com.example.layered.entity.Memo;
+
 public interface MemoRepository {
+
+    /**
+     * Memo를 저장하는 메서드
+     * @param memo - id가 없는 상태로 Repository Layer에 전달된다.
+     * @return Memo
+     */
+    Memo saveMemo(Memo memo);
 }
