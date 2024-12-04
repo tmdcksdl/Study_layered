d5d25c4d6a9e04d0052f29052dba7e7c1b3d032d
tmdcksdl <tmdcksdl99@naver.com>
2024-12-04 11:18:52 +0900

    :sparkles: Fear Override saveMemo Method

--- src/main/java/com/example/layered/repository/MemoRepositoryImpl.java
+++ src/main/java/com/example/layered/repository/MemoRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.example.layered.repository;
 import com.example.layered.entity.Memo;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,4 +22,21 @@ public class MemoRepositoryImpl implements MemoRepository{
      * 그래서 데이터베이스를 넣어준다.
      */
     private final Map<Long, Memo> memoList = new HashMap<>();
+
+
+    @Override
+    public Memo saveMemo(Memo memo) {
+
+        /**
+         * 실제로 데이터베이스와 상호작용을 해야 한다.
+         * memo 식별자(id) 자동 생성
+         */
+        Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
+        memo.setId(memoId);
+
+        memoList.put(memoId, memo);
+
+
+        return memo;
+    }
 }
