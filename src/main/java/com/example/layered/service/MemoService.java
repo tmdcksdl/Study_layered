6c3866cb09053bfb1fb80b6a5ad8a58d4c4f46b9
tmdcksdl <tmdcksdl99@naver.com>
2024-12-04 11:46:58 +0900

    :sparkles: Fear findAllMemos, findMemoById Method

--- src/main/java/com/example/layered/service/MemoService.java
+++ src/main/java/com/example/layered/service/MemoService.java
@@ -3,6 +3,8 @@ package com.example.layered.service;
 import com.example.layered.dto.MemoRequestDto;
 import com.example.layered.dto.MemoResponseDto;
 
+import java.util.List;
+
 public interface MemoService {
     /**
      *
@@ -10,4 +12,8 @@ public interface MemoService {
      * @return MemoResponseDto - 이렇게 해야 MemoController에서 반환 타입이 바로 MemoResponseDto로 들어갈 수 있다. 따로 변환할 필요가 없다.
      */
     MemoResponseDto saveMemo(MemoRequestDto dto);
+
+    List<MemoResponseDto> findAllMemos();
+
+    MemoResponseDto findMemoById(Long id);
 }
