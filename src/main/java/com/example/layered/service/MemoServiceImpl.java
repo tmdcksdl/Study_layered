ba7fd0cdfc7c9acb2d25fac85f6057e23d3d2f5a
tmdcksdl <tmdcksdl99@naver.com>
2024-12-04 10:29:01 +0900

    :memo: Add Comments

--- src/main/java/com/example/layered/service/MemoServiceImpl.java
+++ src/main/java/com/example/layered/service/MemoServiceImpl.java
@@ -1,4 +1,13 @@
 package com.example.layered.service;
 
-public class MemoServiceImpl {
+import org.springframework.stereotype.Service;
+
+/**
+ * Annotation @Service는 @Component와 같다, Spring Bean으로 등록한다는 뜻.
+ * Spring Bean으로 등록되면 다른 클래스에서 주입하여 사용할 수 있다.
+ * 명시적으로 Service Layer 라는것을 나타낸다.
+ * 비지니스 로직을 수행한다.
+ */
+@Service
+public class MemoServiceImpl implements MemoService{
 }
