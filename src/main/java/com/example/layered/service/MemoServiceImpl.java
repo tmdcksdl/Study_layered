dd9652e55c2e512e6dd351c82070a583299c1453
tmdcksdl <tmdcksdl99@naver.com>
2024-12-04 11:20:51 +0900

    :sparkles: Fear saveMemo Method

--- src/main/java/com/example/layered/service/MemoServiceImpl.java
+++ src/main/java/com/example/layered/service/MemoServiceImpl.java
@@ -1,5 +1,9 @@
 package com.example.layered.service;
 
+import com.example.layered.dto.MemoRequestDto;
+import com.example.layered.dto.MemoResponseDto;
+import com.example.layered.entity.Memo;
+import com.example.layered.repository.MemoRepository;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,4 +14,23 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MemoServiceImpl implements MemoService{
+
+    // 항상 이 구조를 가져가니까 잘 기억해둬야 한다.
+    private final MemoRepository memoRepository;
+
+    public MemoServiceImpl(MemoRepository memoRepository) {
+        this.memoRepository = memoRepository;
+    }
+
+    @Override
+    public MemoResponseDto saveMemo(MemoRequestDto dto) {
+
+        // 요청받은 데이터로 Memo 객체 생성 -> 식별자인 ID 값은 없다.
+        Memo memo = new Memo(dto.getTitle(), dto.getContents());
+
+        // DB 저장
+        Memo savedMemo = memoRepository.saveMemo(memo);
+
+        return new MemoResponseDto(savedMemo);
+    }
 }
