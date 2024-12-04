b32b37e1f10d47c78dadcd3e01f9c2d2ba7aae32
tmdcksdl <tmdcksdl99@naver.com>
2024-12-04 11:47:21 +0900

    :sparkles: Override findAllMemos, findMemoById Method

--- src/main/java/com/example/layered/service/MemoServiceImpl.java
+++ src/main/java/com/example/layered/service/MemoServiceImpl.java
@@ -4,7 +4,11 @@ import com.example.layered.dto.MemoRequestDto;
 import com.example.layered.dto.MemoResponseDto;
 import com.example.layered.entity.Memo;
 import com.example.layered.repository.MemoRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 /**
  * Annotation @Service는 @Component와 같다, Spring Bean으로 등록한다는 뜻.
@@ -32,5 +36,27 @@ public class MemoServiceImpl implements MemoService{
         Memo savedMemo = memoRepository.saveMemo(memo);
 
         return new MemoResponseDto(savedMemo);
+    }
+
+    @Override
+    public List<MemoResponseDto> findAllMemos() {
+
+        // 리스트 형태의 MemoResponseDto를 바로 반환받는다.
+        List<MemoResponseDto> allMemos = memoRepository.findAllMemos();
+
+        // return으로 바로 memoRepository.findAllMemos()를 받아도 상관없다. 변수에 저장해서 반환하느냐, 바로 반환하느냐의 차이이다.
+        return allMemos;
+    }
+
+    @Override
+    public MemoResponseDto findMemoById(Long id) {
+
+        Memo memo = memoRepository.findMemoById(id);
+
+        if (memo == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id);
+        }
+
+        return new MemoResponseDto(memo);
     }
 }
