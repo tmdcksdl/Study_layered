91a731a5788466981071735b180b050231ed291f
tmdcksdl <tmdcksdl99@naver.com>
2024-12-04 19:40:17 +0900

    :zap: Fear @AllArgsConstructor

--- src/main/java/com/example/layered/dto/MemoResponseDto.java
+++ src/main/java/com/example/layered/dto/MemoResponseDto.java
@@ -1,9 +1,11 @@
 package com.example.layered.dto;
 
 import com.example.layered.entity.Memo;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor  // 전체 필드를 가진 생성자는 없다. 그래서 전체 필드를 가진 생성자를 추가해준다.
 public class MemoResponseDto {
 
     private Long id;
