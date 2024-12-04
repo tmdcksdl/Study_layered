399e88c56cea13819fc88b84116cbe8f9e0659f4
tmdcksdl <tmdcksdl99@naver.com>
2024-12-04 10:07:42 +0900

    init commit

---
+++ src/main/java/com/example/layered/entity/Memo.java
@@ -0,0 +1,23 @@
+package com.example.layered.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Memo {
+
+    private Long id;
+    private String title;
+    private String contents;
+
+    public void update(MemoRequestDto dto) {
+        this.title = dto.getTitle();
+        this.contents = dto.getContents();
+    }
+
+    public void updateTitle(MemoRequestDto dto) {
+        this.title = dto.getTitle();
+    }
+
+}
