8ec1fb33a10b98a4e591366c8acbf426361f0951
tmdcksdl <tmdcksdl99@naver.com>
2024-12-04 11:21:05 +0900

    :sparkles: Fear saveMemo Method

--- src/main/java/com/example/layered/service/MemoService.java
+++ src/main/java/com/example/layered/service/MemoService.java
@@ -1,4 +1,13 @@
 package com.example.layered.service;
 
+import com.example.layered.dto.MemoRequestDto;
+import com.example.layered.dto.MemoResponseDto;
+
 public interface MemoService {
+    /**
+     *
+     * @param dto
+     * @return MemoResponseDto - 이렇게 해야 MemoController에서 반환 타입이 바로 MemoResponseDto로 들어갈 수 있다. 따로 변환할 필요가 없다.
+     */
+    MemoResponseDto saveMemo(MemoRequestDto dto);
 }
